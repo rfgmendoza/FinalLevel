32ba53b2b1aa435c4aa83b315c2d1a6dce9e0556
wildlotusmedia <wildlotus@mail.com>
2013-11-06 13:09:19 -0500

    Update shipping.php

--- shipping.php
+++ shipping.php
@@ -129,26 +129,27 @@ require_once('db_connect.php');
             <div class="row">
                 <div class="container">
                     <div class="twelvecol">
+                    <div id="paragraph">
 						<h1>Shipping Rates & Delivery Times</h1>
 						<p>Currently finalevelgames.com processes and ships items Monday through Friday. Orders placed on Saturday or Sunday will be processed and/or shipped the following business day.</p>
 						<p>We will not process or ship orders on the following holidays: New Years Day, Memorial Day, Fourth of July, Labor Day, Thanksgiving, and Christmas. Additional processing time may be necessary on bank closure holidays.</p>.
-						<h2>Shipping Methods<h2>
+						<h2>Shipping Methods</h2>
 						<p>We offer three methods for shipping your orders from finalevelgames.com:</p>
 						<ul>
-						<li> <h3>Standard</h3> – 4 to 8 business days
+						<li> <h3>Standard</h3> – 4 to 8 business days</h3>
 						Includes 1 day for processing and 3 to 7 days for transit.</li>
 						 
-						<li> <h3> Expedited </h3> – 3 business days
+						<li> <h3> Expedited </h3> – 3 business days</h3>
 						Includes 1 day for processing and 2 days for transit.</li>
 						 
-						<li> <h3> Express </h3> – 2 business days
+						<li> <h3> Express </h3> – 2 business days</h3>
 						Includes 1 day for processing and 1 day for transit.</li>
 						</ul>
 
 						<p>FinaLevel items can be shipped to most places. FinaLevel items cannot be shipped to:</p>
 						<ul>
 								<li>PO boxes</li>
-								,<li>APO/FPO (military addresses)</li>
+								<li>APO/FPO (military addresses)</li>
 						</ul>
 						<h2>Shipping Carriers</h2>
 								<p>FinaLevel partners with<b> UPS, UPS Mail Innovations and the United States Postal Service (USPS)</b> to ship your packages. Standard shipping orders are shipped by UPS, UPS Mail Innovations or the United States Postal Service (USPS). Expedited or Express orders are shipped by UPS. You'll receive a shipping confirmation e-mail that will confirm which carrier was used for your products.</p>
@@ -158,7 +159,7 @@ require_once('db_connect.php');
 						<h2>Shipping Costs</h2>
 						<p>Shipping cost is calculated on a "per item" or "per pound" basis and is based on the shipping method you select (Standard, Expedited, Express). We sometimes offer free shipping in select categories, or free shipping site-wide.</p>
 
-
+</div>
 					</div>
                 </div>
             </div>
