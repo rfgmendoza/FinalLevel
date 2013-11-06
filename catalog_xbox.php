8fd125b10e3fd429a38c39641dd599bae581daf6
wildlotusmedia <wildlotus@mail.com>
2013-11-06 12:52:34 -0500

    Update catalog_xbox.php

--- catalog_xbox.php
+++ catalog_xbox.php
@@ -36,10 +36,6 @@ if($_GET['action']=='exists'){
 <script type="text/javascript" src="js/jquery.touchSwipe.min.js"></script>
 
 <script type="text/javascript" src="js/custom.js"></script>
-
-</head>
-
-<body>
 <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -50,6 +46,10 @@ if($_GET['action']=='exists'){
   ga('send', 'pageview');
 
 </script>
+</head>
+
+<body>
+
 <div id="page_warp">
     <div id="header">
         <div id="header-top">
