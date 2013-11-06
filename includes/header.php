ee4aab92bf79785bd854c19499efcfbaea6d0c99
wildlotusmedia <wildlotus@mail.com>
2013-11-06 16:42:02 -0500

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,14 +1,4 @@
 <body>
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-41209891-2', 'ucf.edu');
-  ga('send', 'pageview');
-
-</script>
 <div id="page_warp">
     <div id="header">
         <div id="header-top">
