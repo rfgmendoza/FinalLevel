33000566505a3137c3a39cf7e475c63fcef3cd42
wildlotusmedia <wildlotus@mail.com>
2013-11-06 12:56:59 -0500

    Update productpage.php

--- productpage.php
+++ productpage.php
@@ -9,7 +9,7 @@ require_once('db_connect.php');
 <html dir="ltr" lang="en">
 <head>
 <meta charset="UTF-8" />
-<title>Final Level Games Home – Miki Nagai</title>
+<title>Final Level Games Product Page</title>
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
 
@@ -29,7 +29,16 @@ require_once('db_connect.php');
 <script type="text/javascript" src="js/jquery.touchSwipe.min.js"></script>
 
 <script type="text/javascript" src="js/custom.js"></script>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
+  ga('create', 'UA-41209891-2', 'ucf.edu');
+  ga('send', 'pageview');
+
+</script>
 </head>
 
 <body>
@@ -193,7 +202,8 @@ require_once('db_connect.php');
             </div><!--row-->
             <div class="row footer-bottom">
                 <div class="tencol">
-                    <p class="footer-content">This site is not official and is an assignment for a UCF Digital Media course <br />designed by Miki Nagai</p>
+                    <p class="footer-content">This site is not official and is an assignment for a UCF Digital Media course <br />designed by Rafael Mendoza, Melissa McDermott, Ashley Pajak, Miki Nagai, and James Marquis.
+</p>
                 </div>
                 
                 <div class="twocol last"><image src="img/payment.gif" alt="" /></div>
