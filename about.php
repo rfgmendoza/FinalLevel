159885d25c3bd35092ff5e8bbcbf233ba2c65c76
wildlotusmedia <wildlotus@mail.com>
2013-11-06 17:08:59 -0500

    Update about.php

--- about.php
+++ about.php
@@ -2,23 +2,6 @@
 	include ('includes/head.php');
 	include ('includes/header.php');
 ?>
-    <div id="navigation">
-    	<div class="container">
-        	<div class="row">
-            	<div id="mainmenu" class="twelvecol">
-                	<ul>
-                        <li><a href='catalog_playstation.php?pulltable=$pulltable'>PS3</a></li>
-                        <li><a href='catalog_xbox.php?pulltable=$pulltable'>Xbox360</a></li>
-                        <li><a href='catalog_pc.php?pulltable=$pulltable'>PC</a></li>
-                        <li><a href='catalog_wii.php?pulltable=$pulltable'>Wii</a></li>
-                        <li><a href='catalog_nintendods.php?pulltable=$pulltable'>Nintendo DS</a></li>
-                        <li><a href="#">More</a></li>
-                   </ul>
-                </div>
-            </div>
-        </div>
-    </div><!--navigation-->
-    
     <div class="client-content">
     	<div class="container">
         	<div class="row">
