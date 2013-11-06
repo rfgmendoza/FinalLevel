7d7ebf68db7ddd127bf2546a2c6a7aacd478b6d6
wildlotusmedia <wildlotus@mail.com>
2013-11-06 12:50:18 -0500

    Update about.php

--- about.php
+++ about.php
@@ -22,10 +22,6 @@
 <script type="text/javascript" src="js/jquery.touchSwipe.min.js"></script>
 
 <script type="text/javascript" src="js/custom.js"></script>
-
-</head>
-
-<body>
 <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -36,6 +32,9 @@
   ga('send', 'pageview');
 
 </script>
+</head>
+
+<body>
 <div id="page_warp">
 	<div id="header">
     	<div id="header-top">
